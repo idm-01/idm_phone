95e81218f68056e15801b6ab84ce2f83290d2ef0
Roman Rihter <teledurak@gmail.com>
2021-10-31 15:54:17 +0300

    Update predefined path

--- lib/predefined.dart
+++ lib/predefined.dart
@@ -5,19 +5,58 @@ import 'package:idm_phone/data.dart';
 class IdmPredefinedData {
   List<IdmPathPoint> points = [
     IdmPathPoint(
-      pos: LatLng(57.812160, 28.359461),
-      scanDate: DateTime.fromMillisecondsSinceEpoch(1634998541775),
-      iceFill: 75.2,
+        pos: const LatLng(57.858995, 28.210555),
+        scanDate: DateTime.fromMillisecondsSinceEpoch(3381908500000),
+        iceFill: 29.2),
+    IdmPathPoint(
+      pos: const LatLng(57.859464, 28.206308),
+      scanDate: DateTime.fromMillisecondsSinceEpoch(3381908120000),
+      iceFill: 35.5,
     ),
     IdmPathPoint(
-      pos: LatLng(57.812060, 28.359061),
-      scanDate: DateTime.fromMillisecondsSinceEpoch(1634998541875),
-      iceFill: 55.3,
+      pos: const LatLng(57.859026, 28.203033),
+      scanDate: DateTime.fromMillisecondsSinceEpoch(3381908141000),
+      iceFill: 23.1,
     ),
     IdmPathPoint(
-      pos: LatLng(57.811760, 28.358761),
-      scanDate: DateTime.fromMillisecondsSinceEpoch(1634998541975),
-      iceFill: 55.2,
+      pos: const LatLng(57.858353, 28.199596),
+      scanDate: DateTime.fromMillisecondsSinceEpoch(3381908160001),
+      iceFill: 36.2,
+    ),
+    IdmPathPoint(
+      pos: const LatLng(57.857730, 28.195034),
+      scanDate: DateTime.fromMillisecondsSinceEpoch(3381908180004),
+      iceFill: 23.6,
+    ),
+    IdmPathPoint(
+      pos: const LatLng(57.857712, 28.190452),
+      scanDate: DateTime.fromMillisecondsSinceEpoch(3381908200044),
+      iceFill: 17.5,
+    ),
+    IdmPathPoint(
+      pos: const LatLng(57.857712, 28.190452),
+      scanDate: DateTime.fromMillisecondsSinceEpoch(3381908220026),
+      iceFill: 19.8,
+    ),
+    IdmPathPoint(
+      pos: const LatLng(57.857887, 28.184412),
+      scanDate: DateTime.fromMillisecondsSinceEpoch(3381908240016),
+      iceFill: 33.2,
+    ),
+    IdmPathPoint(
+      pos: const LatLng(57.857993, 28.181014),
+      scanDate: DateTime.fromMillisecondsSinceEpoch(3381908261007),
+      iceFill: 28.6,
+    ),
+    IdmPathPoint(
+      pos: const LatLng(57.858145, 28.175920),
+      scanDate: DateTime.fromMillisecondsSinceEpoch(3381908282007),
+      iceFill: 45.1,
+    ),
+    IdmPathPoint(
+      pos: const LatLng(57.857773, 28.172118),
+      scanDate: DateTime.fromMillisecondsSinceEpoch(3381908300330),
+      iceFill: 38.3,
     ),
   ];
 }
